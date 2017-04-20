bba05b7032f890a5633bffbda1d72a228a77e4b7
Victor Tisnado <vtisnado@gmail.com>
2017-04-20 00:36:04 -0500

    Update App.js
    @expo/ex-navigation changed for react-native-router-flux

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,21 @@
 
 import React, { Component } from 'react';
 
-import {
-  NavigationProvider,
-  StackNavigation,
-} from '@exponent/ex-navigation';
-
-import Router from '../Router';
+import {Scene, Router} from 'react-native-router-flux';
+import Feeds from './Feeds';
+import Articles from './Articles';
 
 class App extends Component<void, void, void> {
   navigator: any;
 
   render() {
     return (
-      <NavigationProvider router={Router}>
-        <StackNavigation initialRoute={Router.getRoute('feeds')} />
-      </NavigationProvider>
+      <Router>
+        <Scene key="root">
+          <Scene key="feeds" component={Feeds} hideNavBar />
+          <Scene key="articles" component={Articles} hideNavBar />
+        </Scene>
+      </Router>
     );
   }
 }
